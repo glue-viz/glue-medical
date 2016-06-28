4917a68c552c7e3bfd11d2a2c18f3e4e1faa60da
Thomas Robitaille <thomas.robitaille@gmail.com>
2016-06-28 13:08:41 +0100

    Added initial layout

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+from setuptools import setup, find_packages
+
+entry_points = """
+[glue.plugins]
+glue_medical=glue_medical:setup
+"""
+
+try:
+    import pypandoc
+    LONG_DESCRIPTION = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+    with open('README.md') as infile:
+        LONG_DESCRIPTION = infile.read()
+
+with open('glue_medical/version.py') as infile:
+    exec(infile.read())
+
+setup(name='glue_medical',
+      version=__version__,
+      description='Plugin for glue to support medical data',
+      long_description=LONG_DESCRIPTION,
+      url="https://github.com/glue-viz/glue-medical",
+      author='',
+      author_email='',
+      packages = find_packages(),
+      package_data={},
+      entry_points=entry_points
+    )
