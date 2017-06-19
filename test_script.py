dbaae718525dec55fe9d5cc7c7cf9a94f94cff4e
AndrewBeers <andrew_beers@alumni.brown.edu>
2017-06-19 15:39:16 -0400

    Small change to test script and sync

--- test_script.py
+++ test_script.py
@@ -1,9 +1,15 @@
 from glue_medical import setup
 from glue_medical.nifti.nifti_factory import nifti_reader
-
 from glue_medical.nrrd.nrrd_factory import nrrd_reader
 
 
 setup()
 
-nrrd_reader('MRBrainTumor2.nrrd')
+x = nrrd_reader('MRBrainTumor2.nrrd')
+
+print x
+
+# from glue.core.data_factories.helpers import auto_data
+# auto_data('C:/Users/abeers/Pictures/ModelScreenshot.PNG')
+# auto_data('C:/Users/abeers/Pictures/ModelScreenshot.png')
+# img_data('C:/Users/abeers/Pictures/ModelScreenshot.PNG')
