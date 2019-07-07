2c02de31a08f4698db75e3c8061aa1333cbd44f1
Thomas Robitaille <thomas.robitaille@gmail.com>
2019-07-07 20:25:23 +0100

    Use setuptools_scm for the version

--- glue_medical/__init__.py
+++ glue_medical/__init__.py
@@ -1,3 +1,13 @@
+from pkg_resources import get_distribution, DistributionNotFound
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    __version__ = 'undefined'
+
+__all__ = ['__version__', 'setup']
+
+
 def setup():
-    from .dicom_factory import dicom_reader
-    from .dicom_importer import import_dicom_directory_as_slices
+    from .dicom_factory import dicom_reader  # noqa
+    from .dicom_importer import import_dicom_directory_as_slices  # noqa
